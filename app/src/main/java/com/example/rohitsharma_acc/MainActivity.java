801e18e5acc261126bb36d4529b8db6dac01e291
Rohitdacoder <rohitsharma3860@gmail.com>
2022-12-09 23:05:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rohitsharma_acc/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.rohitsharma_acc;
+
+import static com.example.rohitsharma_acc.R.color.purple_200;
+import static com.example.rohitsharma_acc.R.color.teal_200;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_700)));
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Your age is", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
